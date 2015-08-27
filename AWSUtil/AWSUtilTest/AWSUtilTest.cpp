70b280d906cc028abc5eb6d7009160af95a98ed6
christapley <chris.tapley.81@gmail.com>
2015-08-27 22:46:08 +0800

    upload sample

--- AWSUtil/AWSUtilTest/AWSUtilTest.cpp
+++ AWSUtil/AWSUtilTest/AWSUtilTest.cpp
@@ -45,7 +45,7 @@ void GetS3Object(const std::string &sBucketName, const std::string &sFileKey, co
 	CURL *curl_handle;
 	CURLcode res;
 
-	curl_global_init(CURL_GLOBAL_ALL);
+	
 
 	curl_handle = curl_easy_init();
 	curl_easy_setopt(curl_handle, CURLOPT_URL, sEndpointUrl.c_str());
@@ -121,11 +121,29 @@ void GetPresignedUrlToS3Object(const std::string &sBucketName, const std::string
 	printf("------------------------------------------\n");
 }
 
-void GutS3Object(const std::string &sBucketName, const std::string &sFileKey, const std::string &sRegionName, const std::string &awsAccessKey, const std::string &awsSecretKey)
+
+
+static size_t ReadCallback(void *ptr, size_t size, size_t nmemb, void *pData)
+{
+	std::string *pStr = static_cast<std::string *>(pData);
+	size_t nReadSize = pStr->length() < size*nmemb ? pStr->length() : size*nmemb;
+	memcpy(ptr, pStr->c_str(), nReadSize);
+	return nReadSize;
+}
+
+void PutS3ObjectSample(const std::string &sBucketName, const std::string &sFileKey, const std::string &sRegionName, const std::string &awsAccessKey, const std::string &awsSecretKey)
 {
 	printf("************************************************\n");
 	printf("*        Executing sample 'PutS3Object'        *\n");
 	printf("************************************************\n");
+	
+	std::string sEndpointUrl;
+	if (sRegionName.compare("us-east-1") == 0) {
+		sEndpointUrl = "http://s3.amazonaws.com/" + sBucketName + "/" + sFileKey;
+	}
+	else {
+		sEndpointUrl = "http://s3-" + sRegionName + ".amazonaws.com/" + sBucketName + "/" + sFileKey;
+	}
 
 	std::string sObjectContent =
 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tortor metus, sagittis eget augue ut,\n"
@@ -138,24 +156,17 @@ void GutS3Object(const std::string &sBucketName, const std::string &sFileKey, co
 		"Nunc sollicitudin nunc diam, nec hendrerit dui commodo sed. Duis dapibus commodo elit, id commodo erat\n"
 		"congue id. Aliquam erat volutpat.\n";
 
-	std::string sEndpointUrl;
-	if (sRegionName.compare("us-east-1") == 0) {
-		sEndpointUrl = "https://s3.amazonaws.com/" + sBucketName + "/" + sFileKey;
-	}
-	else {
-		sEndpointUrl = "https://s3-" + sRegionName + ".amazonaws.com/" + sBucketName + "/" + sFileKey;
-	}
-
 	// precompute hash of the body content
 	std::string sObjectContentHash = AWS::Auth::AWS4SignerBase::Hash(sObjectContent);
 
 	std::map<std::string, std::string> Headers, QueryParameters;
 	Headers["x-amz-content-sha256"] = sObjectContentHash;
-	Headers["content-length"] = sObjectContent.length();
+	Headers["content-length"] = std::to_string(sObjectContent.length());
 	Headers["x-amz-storage-class"] = "REDUCED_REDUNDANCY";
+	Headers["Accept"] = "*/*";
+	//Headers["Expect"] = "100-continue";
 
-	AWS::Auth::AWS4SignerForAuthorizationHeader Signer(
-		sEndpointUrl, "PUT", "s3", sRegionName);
+	AWS::Auth::AWS4SignerForAuthorizationHeader Signer(sEndpointUrl, "PUT", "s3", sRegionName);
 
 	std::string sAuthorization = Signer.ComputeSignature(Headers,
 		QueryParameters, // no query parameters
@@ -166,11 +177,38 @@ void GutS3Object(const std::string &sBucketName, const std::string &sFileKey, co
 	// express authorization for this as a header
 	Headers["Authorization"] = sAuthorization;
 
+
+	CURL *curl;
+	CURLcode res;
 	// make the call to Amazon S3
-	//String response = HttpUtils.invokeHttpRequest(endpointUrl, "PUT", headers, objectContent);
-	//System.out.println("--------- Response content ---------");
-	//System.out.println(response);
-	//System.out.println("------------------------------------");
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_READFUNCTION, ReadCallback);
+		curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+		curl_easy_setopt(curl, CURLOPT_PUT, 1L);
+		curl_easy_setopt(curl, CURLOPT_URL, sEndpointUrl.c_str());
+		curl_easy_setopt(curl, CURLOPT_READDATA, &sObjectContent);
+		curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)sObjectContent.length());
+
+		struct curl_slist *headerlist = NULL;
+		std::list<std::string> HeadersList;
+		for (auto it = Headers.cbegin(); it != Headers.cend(); ++it) {
+			HeadersList.push_back(it->first + ": " + it->second);
+		}
+		for (auto it = HeadersList.cbegin(); it != HeadersList.cend(); ++it) {
+			headerlist = curl_slist_append(headerlist, it->c_str());
+		}
+		headerlist = curl_slist_append(headerlist, "Expect:");
+	
+
+		res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+
+		res = curl_easy_perform(curl);
+		if (res != CURLE_OK) {
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+		}
+		curl_easy_cleanup(curl);
+	}
 }
 
 int main(int argc, char **argv)
@@ -186,8 +224,12 @@ int main(int argc, char **argv)
 	std::string sBucket = argv[4];
 	std::string sFileKey = argv[5];
 	
+	curl_global_init(CURL_GLOBAL_ALL);
+	PutS3ObjectSample(sBucket, sFileKey, sRegion, sKey, sSecret);
 	GetS3Object(sBucket, sFileKey, sRegion, sKey, sSecret);
 	GetPresignedUrlToS3Object(sBucket, sFileKey, sRegion, sKey, sSecret);
+	
+	curl_global_cleanup();
     return 0;
 }
 
